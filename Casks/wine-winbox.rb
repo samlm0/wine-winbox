122d610e3dd38170b8d70f48825a317785f43f24
samlm <diaoji@nexfast.com>
2023-07-12 11:42:43 +0800

    add: homebrew cask

---
+++ Casks/wine-winbox.rb
@@ -0,0 +1,15 @@
+cask "wine-winbox" do
+    version "3.38"
+    sha256 :no_check
+
+    depends_on cask: "wine-stable"
+    depends_on macos: ">= :big_sur"
+
+    url "https://github.com/samlm0/wine-winbox/releases/download/#{version}/Winbox.app.zip"
+
+    app "Winbox.app"
+
+    name "MikroTik Winbox"
+    desc "A simple script to start MikroTik Winbox"
+    homepage "https://mikrotik.com/download"
+  end
